e2cf49736a1ee5ebcf0c14d20ea89e63bc1e2170
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-02-18 17:39:11 -0500

    office hours

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,36 @@
-//need to change
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-const isPublicRoute = createRouteMatcher(["/auth/sign-in(.*)", "/auth/sign-up(.*)"]);
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher(['/api/auth/webhook']);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect();
+    await auth.protect()
   }
-});
+})
 
 export const config = {
   matcher: [
-    "/dashboard", 
-    "/((?!_next/static|favicon.ico|.*\\..*).*)", // Excludes static files
-    "/(api|trpc)(.*)", 
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
+}
 
-// import { clerkMiddleware } from "@clerk/nextjs/server";
+//old version
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// const isPublicRoute = createRouteMatcher(["/auth/sign-in(.*)", "/auth/sign-up(.*)"]);
 
-// export default clerkMiddleware();
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     await auth.protect();
+//   }
+// });
 
 // export const config = {
 //   matcher: [
-//     "/dashboard", // ✅ Protects the dashboard
-//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-//     "/(api|trpc)(.*)", // Protects API routes
+//     "/dashboard", 
+//     "/((?!_next/static|favicon.ico|.*\\..*).*)", // Excludes static files
+//     "/(api|trpc)(.*)", 
 //   ],
 // };
 
